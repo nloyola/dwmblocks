52078d5baa36cff5d89176229c79581df29e0157
Nelson Loyola <nloyola@gmail.com>
2021-03-29 20:15:28 +0200

    Give a signal to each block
    This way wrong blocks are not reported as having been clicked.

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    /*Icon*/    /*Command*/                                         /*Update Interval*/     /*Update Signal*/
     {"",        "~/bin/dwm-cpu",                                             10,             1},
-    {"Mem: ",   "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",   30,             0},
-    {"",        "~/bin/dwm-upgrades",                                       600,             0},
+    {"Mem: ",   "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",   30,             4},
+    {"",        "~/bin/dwm-upgrades",                                       600,             5},
     {"",        "~/bin/dwm-volume",                                           0,             2},
     {"",        "~/bin/dwm-weather",                                        600,             3},
-    {"",        "date '+%a %b %d %I:%M%p'",                                   5,             0},
+    {"",        "date '+%a %b %d %I:%M%p'",                                   5,             6},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
